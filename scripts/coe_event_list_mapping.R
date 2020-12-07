e19c9878ebc53cb8c3dae48256f69818efc52337
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-12-07 16:05:39 -0700

    attempt at choropleth map

--- scripts/coe_event_list_mapping.R
+++ scripts/coe_event_list_mapping.R
@@ -6,6 +6,10 @@ library(zipcodeR)
 library(leaflet)
 library(ggthemes)
 library(mapview)
+library(curl)
+library(geojsonio)
+#library(rgdal)
+library(tigris)
 
 # read data
 events <- read_csv("data/raw/event_zip_code.csv",
@@ -19,6 +23,30 @@ events <- read_csv("data/raw/event_zip_code.csv",
 # inspect
 glimpse(events)
 
+# exploratory
+events %>% 
+  group_by(zipcode) %>%
+  summarise(sum_participants = sum(participants)) %>%
+  arrange(desc(sum_participants))
+
+event_stats <- events %>% 
+  group_by(zipcode) %>%
+  summarise(sum_participants = sum(participants),
+            mean_participants = mean(participants),
+            median_participants = median(participants),
+            min_participants = min(participants),
+            max_participants = max(participants),
+            event_count = n()
+            ) %>%
+  arrange(desc(event_count)) %>%
+  drop_na()
+
+glimpse(event_stats)
+
+event_stats %>%
+  ggplot(mapping = aes(sum_participants)) +
+  geom_histogram(fill = "navy", color = "white")
+  
 # mapping ----
 # save zip codes
 # save usa zip code to environment
@@ -78,3 +106,91 @@ mapped_zipcodes_catch <- leaflet(data = zip_code_map_catch) %>%
   addMarkers(~lng, ~lat)
 
 mapped_zipcodes_catch
+
+
+# download geojson az zipcode shapefile from github
+# https://raw.githubusercontent.com/OpenDataDE/State-zip-code-GeoJSON/master/az_arizona_zip_codes_geo.min.json
+curl_download("https://raw.githubusercontent.com/OpenDataDE/State-zip-code-GeoJSON/master/az_arizona_zip_codes_geo.min.json",
+              destfile = "GIS/az_arizona_zip_codes_geo.min.json")
+
+az_zipcodes <- geojson_read("GIS/az_arizona_zip_codes_geo.min.json", what = "sp")
+
+az_zipcodes %>%
+  leaflet() %>%
+  addTiles() %>%
+  addPolygons(popup = az_zipcodes$ZCTA5CE10)
+
+coe_events <- geo_join(az_zipcodes, 
+                       event_stats, 
+                       by_sp= "ZCTA5CE10", 
+                       by_df = "zipcode",
+                       how = "inner")
+
+pal <- colorBin("Reds",
+                domain = coe_events$sum_participants,
+                bins = 10)
+
+popup_sb <- paste0("Participants: ", 
+                   as.character(coe_events$sum_participants))
+
+coe_events %>%
+  leaflet() %>%
+  addProviderTiles(providers$Stamen.TonerLite) %>%
+  addPolygons(data = coe_events,
+              fillColor = ~pal(coe_events$sum_participants),
+              fillOpacity = .7,
+              popup = ~popup_sb,
+              weight = 1) %>%
+  addLegend(pal = pal,
+            values = coe_events$sum_participants)
+
+
+az_spdf <- readOGR(
+  dsn = "GIS/az_arizona_zip_codes_geo.min.json"
+)
+
+mypalette <- colorNumeric( palette="viridis", 
+                           domain=event_stats$sum_participants, 
+                           na.color="transparent")
+mypalette(c(45,43))
+
+leaflet(az_spdf) %>%
+  addTiles() %>%
+  addPolygons(fillColor = ~mypalette(event_stats$sum_participants),
+              stroke = FALSE) %>%
+  addLegend(pal = mypalette,
+            values = ~event_stats$sum_participants)
+
+
+# bins <- c(0, 250, 500, 750, 1000, Inf)
+# pal <- colorBin("YlOrRd", domain = az_map$density, bins = bins)
+# 
+# labels <- sprintf(
+#   "<strong>%s</strong><br/>%g people / mi<sup>2</sup>",
+#   az_map$name, az_map$density
+# ) %>% lapply(htmltools::HTML)
+# 
+# leaflet(data = az_map) %>%
+#   addTiles() %>%
+#   addPolygons(
+#     fillColor = ~pal(density),
+#     weight = 2,
+#     opacity = 1,
+#     color = "white",
+#     dashArray = "3",
+#     fillOpacity = 0.7,
+#     highlight = highlightOptions(
+#       weight = 5,
+#       color = "#666",
+#       dashArray = "",
+#       fillOpacity = 0.7,
+#       bringToFront = TRUE),
+#     label = labels,
+#     labelOptions = labelOptions(
+#       style = list("font-weight" = "normal", padding = "3px 8px"),
+#       textsize = "15px",
+#       direction = "auto")) %>%
+#   addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
+#             position = "bottomright")
+
+

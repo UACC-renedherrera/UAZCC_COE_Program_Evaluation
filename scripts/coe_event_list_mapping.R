42de5cbe71322218717e5e594577859686044662
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-12-09 11:58:07 -0700

    updates; comments; styling; printable choropleth for state & catchment

--- scripts/coe_event_list_mapping.R
+++ scripts/coe_event_list_mapping.R
@@ -8,27 +8,28 @@ library(ggthemes)
 library(mapview)
 library(curl)
 library(geojsonio)
-#library(rgdal)
+# library(rgdal)
 library(tigris)
-#library(ggmap)
-#library(maps)
+# library(ggmap)
+# library(maps)
 library(sf)
 
 # options
 options(tigris_use_cache = TRUE)
 theme_set(theme_hc())
 
-# load zip code data 
+# load zip code data
 zip_db <- zip_code_db
 
 # read data ----
 events <- read_csv("data/raw/event_zip_code.csv",
-                   col_names = c("location", "zipcode", "participants"),
-                   skip = 1,
-                   col_types = cols(
-                     location = col_character(),
-                     zipcode = col_character()
-                   ))
+  col_names = c("location", "zipcode", "participants"),
+  skip = 1,
+  col_types = cols(
+    location = col_character(),
+    zipcode = col_character()
+  )
+)
 
 # inspect
 glimpse(events)
@@ -36,104 +37,160 @@ glimpse(events)
 events <- inner_join(events, zip_db, by = "zipcode")
 
 events <- events %>%
-  select(location,
-         zipcode,
-         participants,
-         county,
-         state,
-         population,
-         lat,
-         lng) %>%
+  select(
+    location,
+    zipcode,
+    participants,
+    county,
+    state,
+    population,
+    lat,
+    lng
+  ) %>%
   drop_na()
 
-# exploratory
+# exploratory ----
 # events by zipcode for entire state
-events_by_zipcode <- events %>% 
+events_by_zipcode <- events %>%
   group_by(zipcode) %>%
-  summarise(sum_participants = sum(participants),
-            mean_participants = mean(participants),
-            median_participants = median(participants),
-            min_participants = min(participants),
-            max_participants = max(participants),
-            event_count = n()
+  arrange(zipcode) %>%
+  drop_na()
+
+# events by zipcode stats for entire state
+events_by_zipcode_stats <- events %>%
+  group_by(zipcode) %>%
+  summarise(
+    sum_participants = sum(participants),
+    mean_participants = mean(participants),
+    median_participants = median(participants),
+    min_participants = min(participants),
+    max_participants = max(participants),
+    event_count = n()
   ) %>%
   arrange(desc(event_count)) %>%
   drop_na()
 
-# events by zipcode for catchment only 
-events_by_zipcode_catchment <- events %>% 
+# events by zipcode for catchment only
+events_by_zipcode_catchment <- events %>%
   filter(county == "Cochise County" |
-           county == "Pima County" |
-           county == "Pinal County" |
-           county == "Yuma County" |
-           county == "Santa Cruz County") %>%
+    county == "Pima County" |
+    county == "Pinal County" |
+    county == "Yuma County" |
+    county == "Santa Cruz County") %>%
   group_by(zipcode) %>%
-  summarise(sum_participants = sum(participants),
-            mean_participants = mean(participants),
-            median_participants = median(participants),
-            min_participants = min(participants),
-            max_participants = max(participants),
-            event_count = n()
+  arrange(zipcode) %>%
+  drop_na()
+
+# events by zipcode stats for catchment only
+events_by_zipcode_catchment_stats <- events %>%
+  filter(county == "Cochise County" |
+    county == "Pima County" |
+    county == "Pinal County" |
+    county == "Yuma County" |
+    county == "Santa Cruz County") %>%
+  group_by(zipcode) %>%
+  summarise(
+    sum_participants = sum(participants),
+    mean_participants = mean(participants),
+    median_participants = median(participants),
+    min_participants = min(participants),
+    max_participants = max(participants),
+    event_count = n()
   ) %>%
   arrange(desc(event_count)) %>%
   drop_na()
 
 # events by county for entire state
-events_by_county <- events %>% 
+events_by_county <- events %>%
   group_by(county) %>%
-  summarise(sum_participants = sum(participants),
-            mean_participants = mean(participants),
-            median_participants = median(participants),
-            min_participants = min(participants),
-            max_participants = max(participants),
-            event_count = n()
+  arrange(county) %>%
+  drop_na()
+
+# events by county stats for entire state
+events_by_county_stats <- events %>%
+  group_by(county) %>%
+  summarise(
+    sum_participants = sum(participants),
+    mean_participants = mean(participants),
+    median_participants = median(participants),
+    min_participants = min(participants),
+    max_participants = max(participants),
+    event_count = n()
   ) %>%
   arrange(desc(event_count)) %>%
   drop_na()
 
-# events by county for catchment only 
-events_by_county_catchment <- events %>% 
+# events by county for catchment only
+events_by_county_catchment <- events %>%
   filter(county == "Cochise County" |
-           county == "Pima County" |
-           county == "Pinal County" |
-           county == "Yuma County" |
-           county == "Santa Cruz County") %>%
+    county == "Pima County" |
+    county == "Pinal County" |
+    county == "Yuma County" |
+    county == "Santa Cruz County") %>%
   group_by(county) %>%
-  summarise(sum_participants = sum(participants),
-            mean_participants = mean(participants),
-            median_participants = median(participants),
-            min_participants = min(participants),
-            max_participants = max(participants),
-            event_count = n()
+  arrange(county) %>%
+  drop_na()
+
+# events by county stats for catchment only
+events_by_county_catchment_stats <- events %>%
+  filter(county == "Cochise County" |
+    county == "Pima County" |
+    county == "Pinal County" |
+    county == "Yuma County" |
+    county == "Santa Cruz County") %>%
+  group_by(county) %>%
+  summarise(
+    sum_participants = sum(participants),
+    mean_participants = mean(participants),
+    median_participants = median(participants),
+    min_participants = min(participants),
+    max_participants = max(participants),
+    event_count = n()
   ) %>%
   arrange(desc(event_count)) %>%
   drop_na()
 
-
-events_by_zipcode %>%
+# histograms
+# all events in AZ by zipcode
+events_by_zipcode_stats %>%
   ggplot(mapping = aes(sum_participants)) +
-  geom_histogram(fill = "navy", color = "white",
-                 binwidth = 100)
+  geom_histogram(
+    fill = "navy", color = "white",
+    binwidth = 100
+  )
 
-events_by_zipcode %>%
+# events in catchment by zipcode
+events_by_zipcode_catchment_stats %>%
+  ggplot(mapping = aes(sum_participants)) +
+  geom_histogram(
+    fill = "navy", color = "white",
+    binwidth = 100
+  )
+
+events_by_zipcode_stats %>%
   ggplot(mapping = aes(mean_participants)) +
-  geom_histogram(fill = "navy", color = "white",
-                 binwidth = 50)
+  geom_histogram(
+    fill = "navy", color = "white",
+    binwidth = 50
+  )
 
-events_by_zipcode %>%
+events_by_zipcode_stats %>%
   ggplot(mapping = aes(median_participants)) +
-  geom_histogram(fill = "navy", color = "white",
-                 binwidth = 50)
+  geom_histogram(
+    fill = "navy", color = "white",
+    binwidth = 50
+  )
 
-events_by_zipcode %>%
+events_by_zipcode_stats %>%
   ggplot(mapping = aes(event_count)) +
-  geom_histogram(fill = "navy", color = "white",
-                 bins = 5)
+  geom_histogram(
+    fill = "navy", color = "white",
+    bins = 5
+  )
 
 # interactive mapping with leaflet ----
 # markers ----
 # showing pins of which zip codes had coe events and activities
-
 
 # zip code list with county
 events %>%
@@ -164,10 +221,10 @@ events_by_zipcode_maricopa_mapped
 # match up zip codes from eval with usa database
 events_by_zipcode_catchment <- events %>%
   filter(county == "Cochise County" |
-           county == "Pima County" |
-           county == "Pinal County" |
-           county == "Yuma County" |
-           county == "Santa Cruz County")
+    county == "Pima County" |
+    county == "Pinal County" |
+    county == "Yuma County" |
+    county == "Santa Cruz County")
 
 # generate map
 events_by_zipcode_catchment_mapped <- leaflet(data = events_by_zipcode_catchment) %>%
@@ -186,166 +243,288 @@ events_by_zipcode_catchment_mapped
 json_az_zipcodes <- geojson_read("GIS/az_arizona_zip_codes_geo.min.json", what = "sp")
 
 # state of AZ; impact area
-json_events_by_zipcode <- geo_join(json_az_zipcodes, 
-                       events_by_zipcode, 
-                       by_sp= "ZCTA5CE10", 
-                       by_df = "zipcode",
-                       how = "inner")
+# combine json with stats
+json_events_by_zipcode <- geo_join(json_az_zipcodes,
+  events_by_zipcode_stats,
+  by_sp = "ZCTA5CE10",
+  by_df = "zipcode",
+  how = "inner"
+)
 
+# set bins
 bins <- c(0, 40, 80, 150, 331, Inf)
 # comment out because it is hard to see the difference
 # pal <- colorBin(c("#81D3EB", "#378DBD", "#1E5288", "#0C234B", "#001C48"),
 #                 domain = coe_events$sum_participants,
 #                 bins = bins)
 
+# set palette
 pal <- colorBin("YlGnBu",
-                domain = json_events_by_zipcode$sum_participants,
-                bins = bins)
+  domain = json_events_by_zipcode$sum_participants,
+  bins = bins
+)
 
-popup_sb <- paste0("Participants: ", 
-                   as.character(json_events_by_zipcode$sum_participants))
+# set popup text
+popup_sb <- paste0(
+  "Participants: ",
+  as.character(json_events_by_zipcode$sum_participants)
+)
 
+# generate leaflet map
 map_impact_sum <- json_events_by_zipcode %>%
   leaflet() %>%
   addProviderTiles(providers$Stamen.TonerLite) %>%
-  addPolygons(data = json_events_by_zipcode,
-              fillColor = ~pal(json_events_by_zipcode$sum_participants),
-              fillOpacity = .8,
-              popup = ~popup_sb,
-              weight = 1,
-              color = ~pal(json_events_by_zipcode$sum_participants)) %>%
-  addLegend(pal = pal,
-            values = json_events_by_zipcode$sum_participants,
-            title = "Total participants")
+  addPolygons(
+    data = json_events_by_zipcode,
+    fillColor = ~ pal(json_events_by_zipcode$sum_participants),
+    fillOpacity = .8,
+    popup = ~popup_sb,
+    weight = 1,
+    color = ~ pal(json_events_by_zipcode$sum_participants)
+  ) %>%
+  addLegend(
+    pal = pal,
+    values = json_events_by_zipcode$sum_participants,
+    title = "Total participants"
+  )
 
 map_impact_sum
 
+# save to disk
 mapshot(map_impact_sum, file = "figures/maps/event_participants_impact_choropleth.png")
 
 # map catchment only
-events_by_zipcode_catchment
+# combine json with stats
+json_events_by_zipcode_catchment <- geo_join(json_az_zipcodes,
+  events_by_zipcode_catchment_stats,
+  by_sp = "ZCTA5CE10",
+  by_df = "zipcode",
+  how = "inner"
+)
 
-events_by_zipcode_catchment %>%
-  ggplot(mapping = aes(x = sum_participants)) +
-  geom_histogram(binwidth = 100)
-
-events_by_zipcode_catchment <- geo_join(json_az_zipcodes, 
-                                        events_by_zipcode_catchment, 
-                       by_sp= "ZCTA5CE10", 
-                       by_df = "zipcode",
-                       how = "inner")
-
+# set bins
 bins <- c(0, 40, 80, 150, 331, Inf)
+
+# set palette
 pal <- colorBin("YlGnBu",
-                domain = events_by_zipcode_catchment$sum_participants,
-                bins = bins)
+  domain = json_events_by_zipcode_catchment$sum_participants,
+  bins = bins
+)
 
-popup_sb <- paste0("Participants: ", 
-                   as.character(events_by_zipcode_catchment$sum_participants))
+# set popup text
+popup_sb <- paste0(
+  "Participants: ",
+  as.character(json_events_by_zipcode_catchment$sum_participants)
+)
 
-map_catchment_sum <- events_by_zipcode_catchment %>%
+# generate leaflet map
+map_catchment_sum <- json_events_by_zipcode_catchment %>%
   leaflet() %>%
   addProviderTiles(providers$Stamen.TonerLite) %>%
-  addPolygons(data = events_by_zipcode_catchment,
-              fillColor = ~pal(events_by_zipcode_catchment$sum_participants),
-              fillOpacity = .8,
-              popup = ~popup_sb,
-              weight = 1,
-              color = "white") %>%
-  addLegend(pal = pal,
-            values = events_by_zipcode_catchment$sum_participants,
-            title = "Total participants")
+  addPolygons(
+    data = json_events_by_zipcode_catchment,
+    fillColor = ~ pal(json_events_by_zipcode_catchment$sum_participants),
+    fillOpacity = .8,
+    popup = ~popup_sb,
+    weight = 1,
+    color = "white"
+  ) %>%
+  addLegend(
+    pal = pal,
+    values = json_events_by_zipcode_catchment$sum_participants,
+    title = "Total participants"
+  )
 
 map_catchment_sum
 
+# save to disk
 mapshot(map_catchment_sum, file = "figures/maps/event_participants_catchment_choropleth.png")
 
 # by county catchment only
-events_by_county_catchment
+# generate sf data for az counties
+sf_az_counties <- counties(state = 04)
 
+# combine json and stats
+sf_events_by_county_catchment_stats <- geo_join(sf_az_counties,
+  events_by_county_catchment_stats,
+  by_sp = "NAMELSAD",
+  by_df = "county",
+  how = "inner"
+)
 
-# by county AZ state
-coe_event_stats <- inner_join(events, zip_db, by = "zipcode")
+# set bins
+bins <- c(0, 40, 80, 150, 500, Inf)
 
-coe_event_stats <- coe_event_stats %>%
-  select(location,
-         zipcode,
-         participants,
-         county)
-
-glimpse(coe_event_stats)
-
-coe_event_stats <- coe_event_stats %>%
-  group_by(county) %>%
-  summarise(sum_participants = sum(participants),
-            mean_participants = mean(participants),
-            median_participants = median(participants),
-            min_participants = min(participants),
-            max_participants = max(participants),
-            event_count = n()
-  ) %>%
-  arrange(desc(event_count)) %>%
-  drop_na()
-
-glimpse(coe_event_stats)
-
-coe_event_stats %>%
-  filter(county != "Maricopa County") %>%
-  ggplot(mapping = aes(x = sum_participants, y = county)) +
-  geom_bar(stat = "identity")
-  
-# map
-az_counties <- counties(state = 04)
-glimpse(az_counties)
-
-coe_event_stats_by_county <- geo_join(az_counties, 
-         coe_event_stats, 
-         by_sp= "NAMELSAD", 
-         by_df = "county",
-         how = "inner")
-
-bins <- c(0, 100, 200, 600, 2000, Inf)
+# set palette
 pal <- colorBin("YlGnBu",
-                domain = coe_event_stats_by_county$sum_participants,
-                bins = bins)
+  domain = sf_events_by_county_catchment_stats$sum_participants,
+  bins = bins
+)
 
-coe_event_stats_by_county_map <- coe_event_stats_by_county %>%
+# set popup
+popup_sb <- paste0(
+  "Participants: ",
+  as.character(sf_events_by_county_catchment_stats$sum_participants)
+)
+
+# generate leaflet map
+map_catchment_counties_sum <- sf_events_by_county_catchment_stats %>%
   leaflet() %>%
   addProviderTiles(providers$Stamen.TonerLite) %>%
-  addPolygons(data = coe_event_stats_by_county,
-              fillColor = ~pal(coe_event_stats_by_county$sum_participants),
-              fillOpacity = .8,
-              weight = 1,
-              color = "white") %>%
-  addLegend(pal = pal,
-            values = coe_event_stats_by_county$sum_participants,
-            title = "Total participants")
+  addPolygons(
+    data = sf_events_by_county_catchment_stats,
+    fillColor = ~ pal(sf_events_by_county_catchment_stats$sum_participants),
+    fillOpacity = .8,
+    popup = ~popup_sb,
+    weight = 1,
+    color = "white"
+  ) %>%
+  addLegend(
+    pal = pal,
+    values = sf_events_by_county_catchment_stats$sum_participants,
+    title = "Total participants"
+  )
 
-coe_event_stats_by_county_map
+map_catchment_counties_sum
 
-mapshot(coe_event_stats_by_county_map, file = "figures/maps/event_participants_impact_counties_choropleth.png")
+# save to disk
+mapshot(map_catchment_counties_sum, file = "figures/maps/event_participants_catchment_counties_choropleth.png")
+
+# by county AZ state
+# combine json and stats
+sf_events_by_county_stats <- geo_join(sf_az_counties,
+  events_by_county_stats,
+  by_sp = "NAMELSAD",
+  by_df = "county",
+  how = "inner"
+)
+
+# set bins
+bins <- c(0, 40, 80, 200, 800, Inf)
+
+# set palette
+pal <- colorBin("YlGnBu",
+  domain = sf_events_by_county_stats$sum_participants,
+  bins = bins
+)
+
+# set popup
+popup_sb <- paste0(
+  "Participants: ",
+  as.character(sf_events_by_county_stats$sum_participants)
+)
+
+# generate leaflet map
+map_state_counties_sum <- sf_events_by_county_stats %>%
+  leaflet() %>%
+  addProviderTiles(providers$Stamen.TonerLite) %>%
+  addPolygons(
+    data = sf_events_by_county_stats,
+    fillColor = ~ pal(sf_events_by_county_stats$sum_participants),
+    fillOpacity = .8,
+    popup = ~popup_sb,
+    weight = 1,
+    color = "white"
+  ) %>%
+  addLegend(
+    pal = pal,
+    values = sf_events_by_county_stats$sum_participants,
+    title = "Total participants"
+  )
+
+map_state_counties_sum
+
+# save to disk
+mapshot(map_state_counties_sum, file = "figures/maps/event_participants_state_counties_choropleth.png")
 
 # printable maps ----
-# data 
+# data
+sf_az_zipcodes <- geojson_sf("GIS/az_arizona_zip_codes_geo.min.json")
 
-az_zipcode_sf <- geojson_sf("GIS/az_arizona_zip_codes_geo.min.json")
+# entire state
+# join sf and stats
+sf_events_by_zipcode_stats <- geo_join(sf_az_zipcodes,
+  events_by_zipcode_stats,
+  by_sp = "ZCTA5CE10",
+  by_df = "zipcode",
+  how = "inner"
+)
 
-coe_event_by_zip_code <- geo_join(az_zipcode_sf, 
-                       event_stats, 
-                       by_sp= "ZCTA5CE10", 
-                       by_df = "zipcode",
-                       how = "inner")
-
-# testing
-# tigris 
-az_counties_tigris <- counties(state = 04)
-class(az_counties_tigris)
-
-az_counties_tigris <- st_as_sf(az_counties_tigris)
-
-coe_event_by_zip_code %>%
+# generate choropleth and save
+sf_events_by_zipcode_stats_map <- sf_events_by_zipcode_stats %>%
   mutate(fill_value = cut_number(sum_participants, n = 5)) %>%
   ggplot() +
-  geom_sf(data = az_counties_tigris) +
-  geom_sf(mapping = aes(fill = fill_value))
+  geom_sf(data = sf_az_counties) +
+  geom_sf(mapping = aes(fill = fill_value, color = fill_value)) +
+  theme_void() +
+  labs(
+    title = "Total Participants by Zip Code",
+    subtitle = "UAZCC COE Outreach and Education Activities",
+    caption = ""
+  ) +
+  scale_fill_brewer(
+    aesthetics = c("color", "fill"),
+    palette = "YlGnBu",
+    name = "Participants",
+    labels = c("<40", "<80", "<150", "<331", ">=331")
+  )
 
+# display
+sf_events_by_zipcode_stats_map
+
+# export to PNG
+ggsave("sf_events_by_zipcode_stats_map.png",
+  device = "png",
+  path = "figures/maps/",
+  dpi = 300,
+  width = 8,
+  units = "in",
+  limitsize = TRUE
+)
+
+# catchment only
+# join sf and stats
+sf_events_by_zipcode_catchment_stats <- geo_join(sf_az_zipcodes,
+  events_by_zipcode_catchment_stats,
+  by_sp = "ZCTA5CE10",
+  by_df = "zipcode",
+  how = "inner"
+)
+
+sf_events_by_zipcode_catchment_stats %>% summarise(
+  min_INTPTLAT10 = min(INTPTLAT10),
+  max_INTPTLAT10 = max(INTPTLAT10)
+)
+
+# generate choropleth and save
+sf_events_by_zipcode_catchment_stats_map <- sf_events_by_zipcode_catchment_stats %>%
+  mutate(fill_value = cut_number(sum_participants, n = 5)) %>%
+  ggplot() +
+  geom_sf(data = sf_az_counties) +
+  geom_sf(mapping = aes(fill = fill_value, color = fill_value)) +
+  coord_sf(ylim = c(31, 33.4)) +
+  theme_void() +
+  labs(
+    title = "Total Participants by Zip Code",
+    subtitle = "UAZCC COE Outreach and Education Activities in the Catchment",
+    caption = ""
+  ) +
+  scale_fill_brewer(
+    aesthetics = c("color", "fill"),
+    palette = "YlGnBu",
+    name = "Participants",
+    labels = c("<40", "<80", "<150", "<331", ">=331")
+  )
+
+# display
+sf_events_by_zipcode_catchment_stats_map
+
+# export to PNG
+ggsave("sf_events_by_zipcode_catchment_stats_map.png",
+  device = "png",
+  path = "figures/maps/",
+  dpi = 300,
+  width = 8,
+  units = "in",
+  limitsize = TRUE
+)
